fb83dc5f95e29601dd7132e4b2156b357ba7af2b
gunnarw <gunnar@wambaugh.org>
2017-05-07 02:14:20 -0700

    Added Zombie movement, added music

--- ZombieFighter/Assets/Scripts/PlayerMovement.cs
+++ ZombieFighter/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,81 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    private Animator anim;
+    private AudioSource audioSrc;
+
+    public AudioClip attackClip;
+    public AudioClip biteClip;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        anim = GetComponent<Animator>();
+        audioSrc = GetComponent<AudioSource>();	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        anim.SetFloat("VertSpeed", Input.GetAxis("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            anim.SetBool("Running", true);
+        }
+        else
+        {
+            anim.SetBool("Running", false);
+        }
+
+        float hor = Input.GetAxis("Horizontal");
+        if (hor > 0 || hor < 0)
+        {
+            transform.Rotate(Vector3.down * Time.deltaTime * hor * -100f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Jumping", true);
+            Invoke("StopJumping", 0.1f);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetLayerWeight(1, 1f);
+            anim.SetInteger("CurrentAction", 3);
+            if (anim.GetInteger("CurrentAction") == 3)
+                audioSrc.PlayOneShot(attackClip);
+
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            anim.SetInteger("CurrentAction", 0);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            anim.SetInteger("CurrentAction", 1);
+            if (anim.GetInteger("CurrentAction") == 1)
+                audioSrc.PlayOneShot(biteClip);
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            anim.SetInteger("CurrentAction", 0);
+        }
+
+        if (Input.GetKeyDown("1"))
+        {
+            if (anim.GetInteger("CurrentAction") == 0)
+            {
+                anim.SetInteger("CurrentAction", 2);
+            } else if (anim.GetInteger("CurrentAction") == 2)
+            {
+                anim.SetInteger("CurrentAction", 0);
+            }
+        }
 	}
+
+    void StopJumping()
+    {
+        anim.SetBool("Jumping", false);
+    }
 }
