93b0b302baa68de4bc75e07122f4fda99685ddd0
gunnarw <gunnar@wambaugh.org>
2017-05-08 03:10:33 -0700

    Added person logic, added more assets

---
+++ ZombieFighter/Assets/Scripts/PersonController.cs
@@ -0,0 +1,340 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class PersonController : MonoBehaviour {
+    private Renderer rend;
+    public float health = 100;
+    public bool edible = false;
+    public AudioClip deathYell;
+    public AudioClip biteYell;
+    public AudioClip[] boneCrunching;
+    public AudioClip blood;
+    public Transform bloodLoc;
+    public GameObject bloodSpray;
+    public float attackSpeed = 2f;
+    public float attackDamage = 15;
+    public Transform trail;
+
+    private float maxHealth = 100;
+    private Animator anim;
+    private bool dead = false;
+    private AudioSource audioSource;
+    private NavMeshAgent agent;
+    private GameObject player;
+    private float attackTimer = 2f;
+    private float stateTimer = 0;
+    private float newStateTime = 10f;
+    private Vector3 fleeLocation;
+    private float correctFleeTimer = 0f;
+    private float correctFleeTimerCap = 1.5f;
+    private bool correctedFlee = false;
+    private bool hidden = false;
+    private PersonState state = PersonState.HIDE;
+    private GameObject[] hidingPlaces;
+    private Vector3 hideLocation;
+    private bool searching = false;
+    private bool hiding = false;
+
+    private float trailTimer = 0f;
+    private float dropTrail = .5f;
+
+    // Use this for initialization
+	void Awake () {
+        anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        hidingPlaces = GameObject.FindGameObjectsWithTag("HidingPlace");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!dead)
+        {
+            switch (state)
+            {
+                case PersonState.FLEE:
+                    FleeMode();
+                    break;
+                case PersonState.HIDE:
+                    HideMode();
+                    break;
+                case PersonState.FIGHT:
+                    FightMode();
+                    break;
+            }
+
+            trailTimer += Time.deltaTime;
+            if (trailTimer >= dropTrail)
+            {
+                trailTimer = 0;
+                Paint(transform.position);
+            }
+        }
+    }
+
+    public void TakeDamage(float amount)
+    {
+        anim.SetLayerWeight(1, 1f);
+        anim.SetTrigger("Hit");
+        health -= amount;
+        audioSource.PlayOneShot(blood);
+        
+        if (health <= 0)
+        {
+            Die();
+        }
+        
+        else if (health / maxHealth <= .4)
+        {
+            SetLayerRecursively(transform);
+            edible = true;
+        }
+
+        FightOrFlight();
+    }
+
+    public void Bitten()
+    {
+        dead = true;
+        anim.SetTrigger("Bitten");
+        Destroy(agent);
+        StartCoroutine(playBoneCracking());
+        GameObject spawnedBlood = Instantiate(bloodSpray, bloodLoc.position, Quaternion.identity);
+        spawnedBlood.transform.parent = gameObject.transform;
+        spawnedBlood.transform.localScale = new Vector3(1, 1, 1);
+        Destroy(spawnedBlood, 15);
+        anim.SetBool("Dead", true);
+        Destroy(gameObject, 15.0f);
+        gameObject.tag = "Untagged";
+        player.GetComponent<PlayerController>().hittable.Remove(gameObject);
+    }
+
+    private void Die()
+    {
+        Destroy(agent);
+        audioSource.PlayOneShot(deathYell);
+        anim.SetTrigger("Die");
+        anim.SetBool("Dead", true);
+        dead = true;
+        Destroy(gameObject, 15.0f);
+        gameObject.tag = "Untagged";
+        player.GetComponent<PlayerController>().hittable.Remove(gameObject);
+    }
+
+    private void SetLayerRecursively(Transform root)
+    {
+        root.gameObject.layer = LayerMask.NameToLayer("Outline");
+        foreach (Transform child in root)
+            SetLayerRecursively(child);
+    }
+
+    IEnumerator playBoneCracking()
+    {
+        audioSource.PlayOneShot(biteYell);
+        yield return new WaitForSeconds(biteYell.length);
+        foreach (AudioClip clip in boneCrunching)
+        {
+            audioSource.PlayOneShot(clip);
+            yield return new WaitForSeconds(clip.length);
+        }
+    }
+
+
+    private void FightMode()
+    {
+        anim.SetBool("Hiding", false);
+        attackTimer += Time.deltaTime;
+        agent.SetDestination(player.transform.position);
+        float dist = Vector3.Distance(player.transform.position,  transform.position);
+        
+        if (dist < 5f && dist > -5f)
+        {
+            anim.SetBool("Running", false);
+            agent.SetDestination(transform.position);
+            AttackPlayer();
+        } else
+        {
+            anim.SetBool("Running", true);
+        }
+    }
+
+    private void FleeMode()
+    {
+        if (CanSeePlayer())
+        {
+            anim.SetBool("Hiding", false);
+            correctFleeTimer += Time.deltaTime;
+            // count down since last checked
+
+            if (!correctedFlee || (agent.velocity.x < 1 && agent.velocity.z < 1))
+            {
+                // run to some random location away from player
+                transform.rotation = Quaternion.LookRotation(transform.position - player.transform.position);
+                fleeLocation = transform.position + transform.forward * Random.Range(15, 20);
+                agent.SetDestination(fleeLocation);
+                correctedFlee = true;
+                anim.SetBool("Running", true);
+                Debug.Log("Fleeing from player");
+            }
+            if (correctFleeTimer >= correctFleeTimerCap)
+            {
+                correctedFlee = false;
+                correctFleeTimer = 0;
+                correctFleeTimerCap = Random.Range(2, 5);
+                Debug.Log("Correcting flee");
+            }
+        } 
+        else
+        {
+            state = PersonState.HIDE;
+        }
+    }
+
+    private void HideMode()
+    {
+        if (CanSeePlayer())
+        {
+            hidden = false;
+            FightOrFlight();
+        } 
+
+        else if (!hidden || !hiding)
+        {
+            if (!hiding)
+            {
+                int rand = Random.Range(0, hidingPlaces.Length);
+                hideLocation = hidingPlaces[rand].transform.position;
+                agent.SetDestination(hideLocation);
+                anim.SetBool("Running", true);
+                Debug.Log("Hiding Run");
+                hiding = true;
+            }
+            else if (hiding)
+            {
+                float dist = Vector3.Distance(hideLocation, transform.position);
+                if (dist < 10f && dist > -10f)
+                {
+                    hidden = true;
+                    anim.SetBool("Hiding", true);
+                    Debug.Log("Hiding 1");
+                }
+                
+            }
+            else if (agent.velocity.x < 3 && agent.velocity.z < 3)
+            {
+                hiding = true;
+                hidden = true;
+                anim.SetBool("Hiding", true);
+                Debug.Log("Hiding 2");
+            }
+        }
+        else
+        {
+            if (stateTimer >= newStateTime)
+            {
+                state = PersonState.FLEE;
+                hidden = false;
+                hiding = false;
+                searching = false;
+                Debug.Log("Hiding Finding new");
+            }
+        }
+    }
+
+    private void AttackPlayer()
+    {
+        if (attackTimer >= attackSpeed)
+        {
+            attackTimer = 0;
+            // hit player
+            Vector3 lookPos = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+            transform.LookAt(lookPos);
+            player.GetComponent<PlayerController>().TakeDamage(attackDamage);
+            anim.SetLayerWeight(2, 1f);
+            anim.SetTrigger("Punch");
+        }
+    }
+
+    private void FightOrFlight()
+    {
+        int rand = Random.Range(0,2);
+        if (rand == 0)
+        {
+            state = PersonState.FIGHT;
+        }
+        else
+        {
+            state = PersonState.FLEE;
+        }
+    }
+
+    private bool CanSeePlayer()
+    {
+        RaycastHit hit;
+        float visibleRange = 50f;
+        Vector3 guardEyes = transform.position;
+        guardEyes.y = 3f;
+
+        if (Vector3.Distance(guardEyes, player.transform.position) < visibleRange)
+        {
+            if (Physics.Raycast(guardEyes, (player.transform.position - guardEyes), out hit, visibleRange))
+            {
+                return hit.transform == player.transform;
+            }
+        }
+        return false;
+    }
+
+    public void Paint(Vector3 location)
+    {
+
+        int n = -1;
+
+        int drops = 1;
+        RaycastHit hit;
+
+        // Generate multiple decals in once
+        while (n <= drops)
+        {
+            n++;
+
+            // Get a random direction (beween -n and n for each vector component)
+            var fwd = transform.TransformDirection(Vector3.down);
+
+            // Raycast around the position to splash everwhere we can
+            if (Physics.Raycast(location, fwd, out hit, 2))
+            {
+                // Create a splash if we found a surface
+                var paintSplatter = GameObject.Instantiate(trail,
+                                                           location,
+
+                                                           // Rotation from the original sprite to the normal
+                                                           // Prefab are currently oriented to z+ so we use the opposite
+                                                           Quaternion.identity) as Transform;
+
+                // Random scale
+                var scaler = Random.Range(.5f, .5f);
+
+                paintSplatter.localScale = new Vector3(
+                    paintSplatter.localScale.x * scaler,
+                    paintSplatter.localScale.y ,
+                    paintSplatter.localScale.z * scaler
+                );
+
+                // Random rotation effect
+                Vector3 paintRot = paintSplatter.transform.rotation.eulerAngles;
+                paintRot = new Vector3(paintRot.x, transform.rotation.eulerAngles.y + 180, paintRot.z);
+                paintSplatter.transform.rotation = Quaternion.Euler(paintRot);
+
+
+                // TODO: What do we do here? We kill them after some sec?
+                Destroy(paintSplatter.gameObject, 180);
+            }
+
+        }
+    }
+}
---
+++ ZombieFighter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Animator anim;
+    private AudioSource audioSrc;
+    private float attackTimer = 1.5f;
+    private bool dead = false;
+
+    public AudioClip attackClip;
+    public AudioClip biteClip;
+    public float attackDamage = 30f;
+    public float attackSpeed = 1.5f;
+    public float maxHealth = 100f;
+    public float health = 100f;
+
+    public List<GameObject> hittable = new List<GameObject>();
+
+	// Use this for initialization
+	void Awake () {
+        anim = GetComponent<Animator>();
+        audioSrc = GetComponent<AudioSource>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        attackTimer += Time.deltaTime;
+
+        // move forward
+        anim.SetFloat("VertSpeed", Input.GetAxis("Vertical"));
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            anim.SetBool("Running", true);
+        }
+        else
+        {
+            anim.SetBool("Running", false);
+        }
+
+        // Look left and right
+        float hor = Input.GetAxis("Horizontal");
+        if (hor > 0 || hor < 0)
+        {
+            transform.Rotate(Vector3.down * Time.deltaTime * hor * -100f);
+        }
+
+        // Jump
+      /*  if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Jumping", true);
+            Invoke("StopJumping", 0.1f);
+        }
+        */
+
+        // Basic Attack
+        if (Input.GetMouseButtonDown(0) && attackTimer >= attackSpeed)
+        {
+            attackTimer = 0.0f;
+            anim.SetLayerWeight(1, 1f);
+            anim.SetInteger("CurrentAction", 3);
+            
+            audioSrc.PlayOneShot(attackClip);
+            if (hittable.Count > 0)
+            {
+                hittable[0].GetComponent<PersonController>().TakeDamage(attackDamage);
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            anim.SetInteger("CurrentAction", 0);
+        }
+
+        // Bite Attack
+        if (Input.GetMouseButtonDown(1) && hittable.Count > 0 && attackTimer >= attackSpeed )
+        {
+            if (hittable[0].GetComponent<PersonController>().edible && !dead)
+            {
+                attackTimer = 0.0f;
+                anim.SetInteger("CurrentAction", 1);
+                if (anim.GetInteger("CurrentAction") == 1)
+                    audioSrc.PlayOneShot(biteClip);
+
+                Vector3 lookPos = new Vector3(hittable[0].transform.position.x, transform.position.y, hittable[0].transform.position.z);
+                transform.LookAt(lookPos);
+
+                hittable[0].GetComponent<Animator>().SetBool("Bitten", true);
+                hittable[0].GetComponent<PersonController>().Bitten();
+                AddHealth(50f);
+            }
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            anim.SetInteger("CurrentAction", 0);
+        }
+
+        // Dance!
+        if (Input.GetKeyDown("1"))
+        {
+            if (anim.GetInteger("CurrentAction") == 0)
+            {
+                anim.SetInteger("CurrentAction", 2);
+            } else if (anim.GetInteger("CurrentAction") == 2)
+            {
+                anim.SetInteger("CurrentAction", 0);
+            }
+        }
+	}
+
+    void StopJumping()
+    {
+        anim.SetBool("Jumping", false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Attackable")
+        {
+            hittable.Add(other.gameObject);
+            Debug.Log("add");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Attackable")
+        {
+            hittable.Remove(other.gameObject);
+            Debug.Log("remove");
+        }
+    }
+
+    private void AddHealth(float amount)
+    {
+        health += amount;
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        Debug.Log(health);
+        if (health <= 0 && !dead)
+        {
+            // die
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        anim.SetTrigger("Die");
+        dead = true;
+    }
+}
